349ce1d5b1263d3f115d5663be71ba786682ad5d
Marcelo Araujo <marcelo.jobs@gmail.com>
2018-05-30 17:48:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# vue-pwa-push-notification
+
+Push notification on a vue pwa project
+works on mobile and desktop
+see example on https://vue-pwa-push-notification.think.art.br
+
+
+
+## Build Setup
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+
+# build for production with minification
+npm run build
---
+++ src/components/Push.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="push">
+    <button  @click="displayNotification">{{msg}}</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Push',
+  data () {
+    return {
+      msg: 'Call Push'
+    }
+  },
+  methods: {
+    displayNotification () {
+      Notification.requestPermission(function(status) {
+        console.log('Notification permission status:', status);
+      });
+      if (Notification.permission === 'granted') {
+        navigator.serviceWorker.getRegistration()
+          .then(function (reg) {
+            if(reg == undefined){
+              console.log("only works online")
+              return
+            }
+            var options = {
+              body: 'First notification!',
+              icon: './static/img/notification-flat.png',
+              vibrate: [100, 50, 100],
+              data: {
+                dateOfArrival: Date.now(),
+                primaryKey: 1
+              },
+              actions: [
+                {
+                  action: 'explore',
+                  title: 'Go to the site',
+                  icon: './static/img/checkmark.png'
+                },
+                {
+                  action: 'close',
+                  title: 'Close the notification',
+                  icon: './static/img/xmark.png'
+                }
+              ]
+            }
+            reg.showNotification('Your Message Here!', options)
+          }
+        )
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
